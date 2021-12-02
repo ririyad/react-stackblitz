3e40a825537502cfb5224442539a5b988eae544d
ririyad <rasheduliriyad@gmail.com>
2021-12-02 12:44:01 +0600

    1 file modified

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+const ele = (
+  <div>
+    <h1>Hello again </h1>
+  </div>
+)
+
+console.log(ele);
+
 ReactDOM.render(<App />, document.getElementById("root"));
